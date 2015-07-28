2c24843ef92bb6c503d993c7f426291c13045017
ludawei <dawei_lu@126.com>
2015-07-28 12:59:20 +0800

    Initial Commit

---
+++ HelloEarth/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HelloEarth
+//
+//  Created by 卢大维 on 15/7/28.
+//  Copyright (c) 2015年 weather. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
